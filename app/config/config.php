dc430748da5913289f864ead26312c54f843b5fa
Danny Avery <danny.w.avery@gmail.com>
2018-01-13 19:11:07 -0700

    Add config.php + autoload libraries in bootstrap.php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost:8888/traversy-media/oop/dannymvc');
+
+// Site Name
+define('SITENAME', 'DannyMVC');
